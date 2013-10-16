07a7762d82ad1a1b6b9e7fd8c73262ff198047b2
anderslime <aemilnielsen@gmail.com>
2013-10-16 10:11:57 +0200

    Added LocationClient
    With possiblity for querying a location list for a provider

--- lib/lokalebasen_api.rb
+++ lib/lokalebasen_api.rb
@@ -4,6 +4,7 @@ require "lokalebasen_api/resource/base"
 require "lokalebasen_api/resource/root"
 require "lokalebasen_api/resource/location"
 require "lokalebasen_api/client"
+require "lokalebasen_api/location_client"
 
 module LokalebasenApi
   def self.client(credentials, service_url)
